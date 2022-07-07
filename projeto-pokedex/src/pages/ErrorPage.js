e71c072b5d323a22f8ab793a55800808246d92b7
Bruno Pereira Assuncao <bruno.assuncao@grupoboticario.com.br>
2022-07-07 09:28:34 -0300

    coordinator global

--- projeto-pokedex/src/pages/ErrorPage.js
+++ projeto-pokedex/src/pages/ErrorPage.js
@@ -1,22 +1,18 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
 import ErrorPhoto from  '../photo/PikachuTriste1.png'
-import {BackToHome} from '../router/coordinator'
-
+import {goToHome} from '../router/coordinator'
+ 
 
 const ErrorPage = () =>{
     const navigate = useNavigate()
-
-    const BackToHome = () => {
-        navigate('/')
-    }    
 
     return(
         <div className="divError">
             <h1>Error 404</h1>
             <img src={ErrorPhoto} alt="Pikachu triste" />
             <h3>Ops, está página não existe!!</h3>
-            <button onClick={BackToHome}>Voltar para inicio</button>
+            <button onClick={() => goToHome(navigate)}>Voltar para inicio</button>
         </div>
     )
     
