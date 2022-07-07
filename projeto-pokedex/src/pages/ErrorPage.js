dca5173c977d417b293968a2f4a884a350061c8d
Bruno Pereira Assuncao <bruno.assuncao@grupoboticario.com.br>
2022-07-07 13:02:08 -0300

    Centralizando components da tela de erro

--- projeto-pokedex/src/pages/ErrorPage.js
+++ projeto-pokedex/src/pages/ErrorPage.js
@@ -2,18 +2,19 @@ import React from "react"
 import { useNavigate } from "react-router-dom";
 import ErrorPhoto from  '../photo/PikachuTriste1.png'
 import {goToHome} from '../router/coordinator'
+import { CenterContiner } from "./styled";
  
 
 const ErrorPage = () =>{
     const navigate = useNavigate()
 
     return(
-        <div className="divError">
+        <CenterContiner>
             <h1>Error 404</h1>
             <img src={ErrorPhoto} alt="Pikachu triste" />
             <h3>Ops, está página não existe!!</h3>
             <button onClick={() => goToHome(navigate)}>Voltar para inicio</button>
-        </div>
+        </CenterContiner>
     )
     
     }
--- projeto-pokedex/src/pages/styled.js
+++ projeto-pokedex/src/pages/styled.js
@@ -6,3 +6,15 @@ export const ScreenContainer = styled.div`
   flex-wrap: wrap;
 
 `;
+
+export const CenterContiner = styled.div`
+  display: ;
+  text-align: center;
+  align-items: center;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  margin-right: -50%;
+  transform: translate(-50%, -50%)
+
+`;
