96bed9261c091d782c20afd12eae68959128a25d
Bruno Pereira Assuncao <bruno.assuncao@grupoboticario.com.br>
2022-07-07 09:13:57 -0300

    Page error em desenvolvimento

--- projeto-pokedex/src/pages/ErrorPage.js
+++ projeto-pokedex/src/pages/ErrorPage.js
@@ -1,10 +1,22 @@
 import React from "react"
 import { useNavigate } from "react-router-dom";
-
+import ErrorPhoto from  '../photo/PikachuTriste1.png'
+import {BackToHome} from '../router/coordinator'
 
 const ErrorPage = () =>{
+    const navigate = useNavigate()
+
+    const BackToHome = () => {
+        navigate('/')
+    }    
+
     return(
-        <h1>error Page</h1>
+        <div className="divError">
+            <h1>Error 404</h1>
+            <img src={ErrorPhoto} alt="Pikachu triste" />
+            <h3>Ops, está página não existe!!</h3>
+            <button onClick={BackToHome}>Voltar para inicio</button>
+        </div>
     )
     
     }
