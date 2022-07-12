c4b7e670b7c44f6c9a7882d5651e5d22b58fff2a
Danyelleac <danyelleacandido@gmail.com>
2022-07-12 12:46:22 -0300

    subindo alterações no código

--- projeto-pokedex/src/router/router.js
+++ projeto-pokedex/src/router/router.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ErrorPage from "../pages/ErrorPage";
 import DetailsPage from "../pages/DetailsPage";
@@ -9,14 +9,14 @@ function Router() {
   const [pokedex, setpokedex] = useState([]);
 
     return (
-        <BrowserRouter>
+
           <Routes>
               <Route exact path='/' element={<HomePage pokedex={pokedex} setpokedex={setpokedex}/>}/> 
               <Route exact path='/details' element={<DetailsPage/>}/>   
               <Route exact path='/pokedex' element={<PokedexPage pokedex={pokedex} setpokedex={setpokedex}/>}/>     
               <Route path='*' element={<ErrorPage/>}/>
           </Routes>
-        </BrowserRouter>
+
     );
   }
   export default Router;
