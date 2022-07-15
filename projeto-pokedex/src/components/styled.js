8a319591429e1452b2303a35e73e84c6c531df46
Danyelleac <danyelleacandido@gmail.com>
2022-07-15 18:23:13 -0300

    alterações finais

--- projeto-pokedex/src/components/styled.js
+++ projeto-pokedex/src/components/styled.js
@@ -1,18 +1,20 @@
 import styled from "styled-components";
 import image from '../assets/image.jpg';
+import Pokeball from '../assets/Pokeball.png';
 
 export const CardContainer = styled.div`
   border: 2px solid black;
   width: 220px;
-  height: 270px;
+  height: 320px;
   display: flex;
   flex-direction: column;
   align-items: center;
   margin: 20px;
-  background-image: url(${image});
+  background-image: url(${image}) ;
   background-size: cover;
   border-color: #97bfb4;
   border-radius: 10%;
+  padding: 5px;
 `;
 
 export const ContainerImg = styled.div`
@@ -63,7 +65,7 @@ export const CardContainerMove = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 10px;
+  margin: 5px;
   background-color: #97bfb4;
   border-color: #97bfb4;
   border-radius: 2%;
@@ -76,17 +78,36 @@ export const CardContainerTypes = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 10px;
+  margin: 5px;
   background-color: #97bfb4;
   border-color: #97bfb4;
   border-radius: 2%;
 `;
 
 export const PokemonImage = styled.img`
-  width: 100%;
-  height: 250px;
+  width: 80%;
+  height: 200px;
 `;
 
 export const NamePokemon = styled.h2`
-  margin: 5px 0;
+  color: #fbca04;
+  text-shadow:  0 0 0.2em blue, 0 0 0.2em blue,
+        0 0 0.2em blue ;
 `;
+
+export const PokeButton = styled.button`
+  background-image: url(${Pokeball});
+  background-size: 48px 48px;
+  margin:10px;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  border: 1px solid white;
+
+/*   font-weight:bolder;
+  font-size: 40px;
+  color: #fbca04; */
+`
+
+
+
